f802a48435ae36f9d1670bc636fedf2aeb9b5130
BL1183757 <bl1183757@gmail.com>
2026-05-14 11:33:50 +0530

    Add API entrypoint and restore vercel.json

---
+++ api/index.js
@@ -0,0 +1,3 @@
+// Placeholder for Vercel deployment. 
+// This file will be replaced by the actual bundle during the build process.
+export default (req, res) => { res.status(200).send('Building...'); };
